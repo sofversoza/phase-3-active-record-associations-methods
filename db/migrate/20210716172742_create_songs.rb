34715d062053e7d2516218a3e31a0111388445ba
sofiaversoza <sofversoza@gmail.com>
2022-01-20 18:46:00 -0700

    commit tree f5e1abc3e92226a2d73a80a3f005cb815ef45638

--- db/migrate/20210716172742_create_songs.rb
+++ db/migrate/20210716172742_create_songs.rb
@@ -1,7 +1,7 @@
 class CreateSongs < ActiveRecord::Migration[6.1]
   def change
     create_table :songs do |t|
-      t.string :name
+      t.string  :name
       t.integer :artist_id
       t.integer :genre_id
     end
